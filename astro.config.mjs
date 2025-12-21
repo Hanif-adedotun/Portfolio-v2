18b330c92d89c10d185b22af82c1346357819829
Hanif-adedotun <hanif.adedotun@gmail.com>
2025-12-21 23:42:47 +0100

    added certification screen

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), icon()],
 });
